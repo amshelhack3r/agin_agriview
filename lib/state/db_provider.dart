0774f6630052317ee230c7bfb6052e369b4398a3
amshel <amshelhack3r@gmail.com>
2021-02-26 03:50:32 +0300

    Add more types for provider

--- lib/state/db_provider.dart
+++ lib/state/db_provider.dart
@@ -2,29 +2,45 @@ import 'package:flutter/material.dart';
 
 import '../models/county.dart';
 import '../models/cultivation_mode.dart';
+import '../models/market_listings_meta.dart';
+import '../models/produce_status.dart';
 import '../models/unit_type.dart';
 
 class DatabaseProvider with ChangeNotifier {
   List<County> _county;
   List<CultivationMode> _mode;
   List<UnitType> _types;
+  List<Grade> _gradeList;
+  List<ProduceStatus> _produceStatus;
 
   List<County> get county => _county;
-  get modes => _mode;
-  get unitType => _types;
+  List<CultivationMode> get modes => _mode;
+  List<UnitType> get unitType => _types;
+  List<Grade> get grades => _gradeList;
+  List<ProduceStatus> get productStatus => _produceStatus;
 
-  set county(List<County> county) {
+  set setCounty(List<County> county) {
     _county = county;
     notifyListeners();
   }
 
-  set modes(List<CultivationMode> mode) {
+  set setModes(List<CultivationMode> mode) {
     _mode = mode;
     notifyListeners();
   }
 
-  set unitType(List<UnitType> types) {
+  set setUnitType(List<UnitType> types) {
     _types = types;
+    notifyListeners();
+  }
+
+  set setGrade(List<Grade> grades) {
+    _gradeList = grades;
+    notifyListeners();
+  }
+
+  set setProductStatus(List<ProduceStatus> statuses) {
+    _produceStatus = statuses;
     notifyListeners();
   }
 }
