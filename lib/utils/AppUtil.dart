09b78c85474aafbe0a5699143dcbd36b678247ab
amshel <amshelhack3r@gmail.com>
2021-04-05 07:02:39 +0300

    add logic for image compression

--- lib/utils/AppUtil.dart
+++ lib/utils/AppUtil.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:intl/intl.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 class AppUtil {
@@ -33,14 +35,38 @@ class AppUtil {
 
   static Future<String> getImageAsBase64(File _image) async {
     print('length -- ${_image.length()}');
+    var compressedFile = await testCompressAndGetFile(_image);
     try {
-      List<int> imageBytes = await _image.readAsBytes();
+      List<int> imageBytes = await compressedFile.readAsBytes();
       print('length2 -- ${imageBytes.length}');
       return base64Encode(imageBytes);
     } catch (e) {
       print('error -- ${e.toString()}');
     }
     return null;
+  }
+
+  static Future<String> get _localPath async {
+    final directory = await getTemporaryDirectory();
+    return directory.path;
+  }
+
+  //compress file
+  // 2. compress file and get file.
+  static Future<File> testCompressAndGetFile(File file) async {
+    var tempPath = await _localPath;
+    print(tempPath);
+    var result = await FlutterImageCompress.compressAndGetFile(
+      file.absolute.path,
+      "$tempPath.jpg",
+      quality: 88,
+      rotate: 180,
+    );
+
+    print(file.lengthSync());
+    print(result.lengthSync());
+
+    return result;
   }
 
   static formatMobileNumber(String number) {
